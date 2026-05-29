2dfc536415cee2704a06b6cb8bdcc8a167795d98
xadezv <xadezv@gmail.com>
2026-05-30 01:56:07 +0300

    TLS DPI fix: Chrome/Firefox coin-flip, ECH (0xfe0d), GREASE, permutable extensions, debug logging

--- Telegram/SourceFiles/mtproto/details/mtproto_tls_socket.cpp
+++ Telegram/SourceFiles/mtproto/details/mtproto_tls_socket.cpp
@@ -4,6 +4,7 @@ the official desktop application for the Telegram messaging service.
 
 For license and copyright information please follow this link:
 https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+--- DPI FIX by Qz3rK v2 ---
 */
 #include "mtproto/details/mtproto_tls_socket.h"
 
@@ -11,17 +12,22 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/openssl_help.h"
 #include "base/bytes.h"
 #include "base/invoke_queued.h"
-#include "base/random.h"
 #include "base/unixtime.h"
 
 #include <QtCore/QtEndian>
 #include <range/v3/algorithm/reverse.hpp>
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/algorithm/shuffle.hpp>
+#include "base/debug_log.h"
+#include <random>
+#include <thread>
 
 namespace MTP::details {
 namespace {
 
 constexpr auto kMaxGrease = 8;
-constexpr auto kClientHelloLimit = 2048;
+
+constexpr auto kClientHelloLength = 517;
 constexpr auto kHelloDigestLength = 32;
 constexpr auto kLengthSize = sizeof(uint16);
 const auto kServerHelloPart1 = qstr("\x16\x03\x03");
@@ -35,7 +41,22 @@ const auto kClientHeader = qstr("\x17\x03\x03");
 using BigNum = openssl::BigNum;
 using BigNumContext = openssl::Context;
 
+[[nodiscard]] MTPTlsClientHello PrepareClientHelloRulesChrome();
+[[nodiscard]] MTPTlsClientHello PrepareClientHelloRulesFirefox();
+
 [[nodiscard]] MTPTlsClientHello PrepareClientHelloRules() {
+	static const auto kChrome = PrepareClientHelloRulesChrome();
+	static const auto kFirefox = PrepareClientHelloRulesFirefox();
+
+	static std::mt19937 rng(std::random_device{}());
+	static std::uniform_int_distribution<int> dist(0, 1);
+
+	const auto useChrome = (dist(rng) == 0);
+	DEBUG_LOG(("TLS DPI: ClientHello profile selected: %1").arg(useChrome ? "Chrome" : "Firefox"));
+	return useChrome ? kChrome : kFirefox;
+}
+
+[[nodiscard]] MTPTlsClientHello PrepareClientHelloRulesChrome() {
 	using Scope = QVector<MTPTlsBlock>;
 	using Permutation = std::vector<Scope>;
 	using StackElement = std::variant<Scope, Permutation>;
@@ -68,15 +89,6 @@ using BigNumContext = openssl::Context;
 	};
 	const auto K = [&] {
 		pushToBack(MTP_tlsBlockPublicKey());
-	};
-	const auto M = [&] {
-		pushToBack(MTP_tlsBlockM());
-	};
-	const auto E = [&] {
-		pushToBack(MTP_tlsBlockE());
-	};
-	const auto P = [&] {
-		pushToBack(MTP_tlsBlockPadding());
 	};
 	const auto OpenScope = [&] {
 		stack.emplace_back(Scope());
@@ -122,23 +134,20 @@ using BigNumContext = openssl::Context;
 
 	stack.emplace_back(Scope());
 
-	S("\x16\x03\x01"_q);
-	OpenScope();
-	S("\x01\x00"_q);
-	OpenScope();
-	S("\x03\x03"_q);
+	S("\x16\x03\x01\x02\x00\x01\x00\x01\xfc\x03\x03"_q);
 	Z(32);
 	S("\x20"_q);
 	R(32);
+
 	S("\x00\x20"_q);
 	G(0);
 	S(""
 		"\x13\x01\x13\x02\x13\x03\xc0\x2b\xc0\x2f\xc0\x2c\xc0\x30\xcc\xa9"
 		"\xcc\xa8\xc0\x13\xc0\x14\x00\x9c\x00\x9d\x00\x2f\x00\x35\x01\x00"
-		""_q);
-	OpenScope();
+		"\x01\x93"_q);
 	G(2);
 	S("\x00\x00"_q);
+
 	OpenPermutation(); {
 		StartPermutationElement(); {
 			S("\x00\x00"_q);
@@ -155,9 +164,9 @@ using BigNumContext = openssl::Context;
 			S("\x00\x05\x00\x05\x01\x00\x00\x00\x00"_q);
 		}
 		StartPermutationElement(); {
-			S("\x00\x0a\x00\x0c\x00\x0a"_q);
+			S("\x00\x0a\x00\x0a\x00\x08"_q);
 			G(4);
-			S("\x11\xec\x00\x1d\x00\x17\x00\x18"_q);
+			S("\x00\x1d\x00\x17\x00\x18"_q);
 		}
 		StartPermutationElement(); {
 			S("\x00\x0b\x00\x02\x01\x00"_q);
@@ -193,42 +202,197 @@ using BigNumContext = openssl::Context;
 			S("\x00\x2d\x00\x02\x01\x01"_q);
 		}
 		StartPermutationElement(); {
-			S("\x00\x33\x04\xef\x04\xed"_q);
+			S("\x00\x33\x00\x2b\x00\x29"_q);
 			G(4);
-			S("\x00\x01\x00\x11\xec\x04\xc0"_q);
-			M();
-			K();
-			S("\x00\x1d\x00\x20"_q);
+			S("\x00\x01\x00\x00\x1d\x00\x20"_q);
 			K();
 		}
 		StartPermutationElement(); {
-			S("\x44\xcd\x00\x05\x00\x03\x02\x68\x32"_q);
-		}
-		StartPermutationElement(); {
-			S("\xfe\x0d"_q);
-			OpenScope();
-			S("\x00\x00\x01\x00\x01"_q);
-			R(1);
-			S("\x00\x20"_q);
-			R(32);
-			OpenScope();
-			E();
-			CloseScope();
-			CloseScope();
+			S("\x44\x69\x00\x05\x00\x03\x02\x68\x32"_q);
 		}
 		StartPermutationElement(); {
 			S("\xff\x01\x00\x01\x00"_q);
 		}
+		StartPermutationElement(); {
+		    S("\xfe\x0d\x00\x00"_q);
+		}
 	} ClosePermutation();
+
 	G(3);
-	S("\x00\x01\x00"_q);
-	P();
-	CloseScope();
-	CloseScope();
-	CloseScope();
+	S("\x00\x01\x00\x00\x15"_q);
+	Z(83);
 
 	return MTP_tlsClientHello(MTP_vector<MTPTlsBlock>(Finish()));
 }
+
+[[nodiscard]] MTPTlsClientHello PrepareClientHelloRulesFirefox() {
+	using Scope = QVector<MTPTlsBlock>;
+	using Permutation = std::vector<Scope>;
+	using StackElement = std::variant<Scope, Permutation>;
+	auto stack = std::vector<StackElement>();
+	const auto pushToBack = [&](MTPTlsBlock &&block) {
+		Expects(!stack.empty());
+
+		if (const auto scope = std::get_if<Scope>(&stack.back())) {
+			scope->push_back(std::move(block));
+		} else {
+			auto &permutation = v::get<Permutation>(stack.back());
+			Assert(!permutation.empty());
+			permutation.back().push_back(std::move(block));
+		}
+	};
+	const auto S = [&](QByteArray data) {
+		pushToBack(MTP_tlsBlockString(MTP_bytes(data)));
+	};
+	const auto Z = [&](int length) {
+		pushToBack(MTP_tlsBlockZero(MTP_int(length)));
+	};
+	const auto G = [&](int seed) {
+		pushToBack(MTP_tlsBlockGrease(MTP_int(seed)));
+	};
+	const auto R = [&](int length) {
+		pushToBack(MTP_tlsBlockRandom(MTP_int(length)));
+	};
+	const auto D = [&] {
+		pushToBack(MTP_tlsBlockDomain());
+	};
+	const auto K = [&] {
+		pushToBack(MTP_tlsBlockPublicKey());
+	};
+	const auto OpenScope = [&] {
+		stack.emplace_back(Scope());
+	};
+	const auto CloseScope = [&] {
+		Expects(stack.size() > 1);
+		Expects(v::is<Scope>(stack.back()));
+
+		const auto blocks = std::move(v::get<Scope>(stack.back()));
+		stack.pop_back();
+		pushToBack(MTP_tlsBlockScope(MTP_vector<MTPTlsBlock>(blocks)));
+	};
+	const auto OpenPermutation = [&] {
+		stack.emplace_back(Permutation());
+	};
+	const auto ClosePermutation = [&] {
+		Expects(stack.size() > 1);
+		Expects(v::is<Permutation>(stack.back()));
+
+		const auto list = std::move(v::get<Permutation>(stack.back()));
+		stack.pop_back();
+
+		const auto wrapped = list | ranges::views::transform([](
+				const QVector<MTPTlsBlock> &elements) {
+			return MTP_vector<MTPTlsBlock>(elements);
+		}) | ranges::to<QVector<MTPVector<MTPTlsBlock>>>();
+
+		pushToBack(MTP_tlsBlockPermutation(
+			MTP_vector<MTPVector<MTPTlsBlock>>(wrapped)));
+	};
+	const auto StartPermutationElement = [&] {
+		Expects(stack.size() > 1);
+		Expects(v::is<Permutation>(stack.back()));
+
+		v::get<Permutation>(stack.back()).emplace_back();
+	};
+	const auto Finish = [&] {
+		Expects(stack.size() == 1);
+		Expects(v::is<Scope>(stack.back()));
+
+		return v::get<Scope>(stack.back());
+	};
+
+	stack.emplace_back(Scope());
+
+	S("\x16\x03\x01\x02\x00\x01\x00\x01\xfc\x03\x03"_q);
+	Z(32);
+	S("\x20"_q);
+	R(32);
+
+	S("\x00\x20"_q);
+	G(0);
+	S(""
+		"\x13\x01\x13\x03\x13\x02\xc0\x2b\xc0\x2f\xcc\xa9\xcc\xa8\xc0\x2c"
+		"\xc0\x30\xc0\x13\xc0\x14\x00\x9c\x00\x9d\x00\x2f\x00\x35"_q);
+	G(2);
+	S("\x01\x00"_q);
+
+	OpenPermutation(); {
+		StartPermutationElement(); {
+			S("\x00\x00"_q);
+			OpenScope();
+			OpenScope();
+			S("\x00"_q);
+			OpenScope();
+			D();
+			CloseScope();
+			CloseScope();
+			CloseScope();
+		}
+		StartPermutationElement(); {
+			S("\x00\x17\x00\x00"_q);
+		}
+		StartPermutationElement(); {
+			S("\xff\x01\x00\x01\x00"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x0a\x00\x0a\x00\x08"_q);
+			G(4);
+			S("\x00\x1d\x00\x17\x00\x18"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x0b\x00\x02\x01\x00"_q);
+		}
+		StartPermutationElement(); {
+			S(""
+				"\x00\x0d\x00\x12\x00\x10\x04\x03\x08\x04\x04\x01\x05\x03"
+				"\x08\x05\x05\x01\x08\x06\x06\x01"_q);
+		}
+		StartPermutationElement(); {
+			S(""
+				"\x00\x10\x00\x0e\x00\x0c\x02\x68\x32\x08\x68\x74\x74\x70"
+				"\x2f\x31\x2e\x31"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x12\x00\x00"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x1b\x00\x03\x02\x00\x02"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x23\x00\x00"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x2b\x00\x07\x06"_q);
+			G(6);
+			S("\x03\x04\x03\x03"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x2d\x00\x02\x01\x01"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x33\x00\x2b\x00\x29"_q);
+			G(4);
+			S("\x00\x01\x00\x00\x1d\x00\x20"_q);
+			K();
+		}
+		StartPermutationElement(); {
+			S("\x44\x69\x00\x05\x00\x03\x02\x68\x32"_q);
+		}
+		StartPermutationElement(); {
+			S("\x00\x05\x00\x05\x01\x00\x00\x00\x00"_q);
+		}
+		StartPermutationElement(); {
+			S("\xfe\x0d\x00\x00"_q);
+		}
+	} ClosePermutation();
+
+	G(3);
+	S("\x00\x01\x00\x00\x15"_q);
+	Z(83);
+
+	return MTP_tlsClientHello(MTP_vector<MTPTlsBlock>(Finish()));
+}
+
 
 [[nodiscard]] bytes::vector PrepareGreases() {
 	auto result = bytes::vector(kMaxGrease);
@@ -242,44 +406,85 @@ using BigNumContext = openssl::Context;
 			result[i + 1] = bytes::type(uchar(result[i + 1]) ^ 0x10);
 		}
 	}
+	for (auto i = 0; i != kMaxGrease; ++i) {
+		const auto j = (uchar(result[i]) * 0x9E3779B9) % kMaxGrease;
+		if (i != j) {
+			std::swap(result[i], result[j]);
+		}
+	}
 	return result;
 }
 
+[[nodiscard]] BigNum GenerateY2(
+		const BigNum &x,
+		const BigNum &mod,
+		const BigNumContext &context) {
+	auto coef = BigNum(486662);
+	auto y = BigNum::ModAdd(x, coef, mod, context);
+	y.setModMul(y, x, mod, context);
+	coef.setWord(1);
+	y.setModAdd(y, coef, mod, context);
+	return BigNum::ModMul(y, x, mod, context);
+}
+
+[[nodiscard]] BigNum GenerateX2(
+		const BigNum &x,
+		const BigNum &mod,
+		const BigNumContext &context) {
+	auto denominator = GenerateY2(x, mod, context);
+	auto coef = BigNum(4);
+	denominator.setModMul(denominator, coef, mod, context);
+
+	auto numerator = BigNum::ModMul(x, x, mod, context);
+	coef.setWord(1);
+	numerator.setModSub(numerator, coef, mod, context);
+	numerator.setModMul(numerator, numerator, mod, context);
+
+	denominator.setModInverse(denominator, mod, context);
+	return BigNum::ModMul(numerator, denominator, mod, context);
+}
+
 [[nodiscard]] bytes::vector GeneratePublicKey() {
-	const auto context = EVP_PKEY_CTX_new_id(NID_ED25519, nullptr);
-	if (!context) {
-		return {};
-	}
-	const auto guardContext = gsl::finally([&] {
-		EVP_PKEY_CTX_free(context);
-	});
+	const auto context = BigNumContext();
+	const char modBytes[] = ""
+		"\x7f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+		"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xed";
+	const char powBytes[] = ""
+		"\x3f\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+		"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf6";
+	const auto mod = BigNum(bytes::make_span(modBytes).subspan(0, 32));
+	const auto pow = BigNum(bytes::make_span(powBytes).subspan(0, 32));
 
-	if (EVP_PKEY_keygen_init(context) <= 0) {
-		return {};
-	}
+	auto x = BigNum();
+	do {
+		while (true) {
+			auto random = bytes::vector(32);
+			bytes::set_random(random);
+			random[31] &= bytes::type(0x7FU);
+			x.setBytes(random);
+			x.setModMul(x, x, mod, context);
 
-	auto key = (EVP_PKEY*)nullptr;
-	if (EVP_PKEY_keygen(context, &key) <= 0) {
-		return {};
-	}
-	const auto guardKey = gsl::finally([&] {
-		EVP_PKEY_free(key);
-	});
+			auto y = GenerateY2(x, mod, context);
+			if (BigNum::ModExp(y, pow, mod, context).isOne()) {
+				break;
+			}
+		}
+		for (auto i = 0; i != 3; ++i) {
+			x = GenerateX2(x, mod, context);
+		}
+		const auto xBytes = x.getBytes();
+		Assert(!xBytes.empty());
+		Assert(xBytes.size() <= 32);
+	} while (x.bytesSize() == 32);
 
-	auto length = size_t(0);
-	if (!EVP_PKEY_get_raw_public_key(key, nullptr, &length)) {
-		return {};
-	}
-	Assert(length == 32);
+	const auto xBytes = x.getBytes();
+	auto result = bytes::vector(32, bytes::type());
+	bytes::copy(
+		bytes::make_span(result).subspan(32 - xBytes.size()),
+		xBytes);
+	ranges::reverse(result);
 
-	auto result = bytes::vector(length);
-	const auto code = EVP_PKEY_get_raw_public_key(
-		key,
-		reinterpret_cast<unsigned char *>(result.data()),
-		&length);
-	if (!code) {
-		return {};
-	}
+
 	return result;
 }
 
@@ -324,6 +529,7 @@ private:
 		[[nodiscard]] QByteArray take();
 
 	private:
+		void writePadding();
 		void writeDigest(bytes::const_span key);
 		void injectTimestamp();
 
@@ -347,7 +553,7 @@ Generator::Part::Part(
 	const bytes::vector &greases)
 : _domain(domain)
 , _greases(greases) {
-	_result.reserve(kClientHelloLimit);
+	_result.reserve(kClientHelloLength);
 	_data = _result.constData();
 }
 
@@ -364,7 +570,7 @@ QByteArray Generator::Part::take() {
 bytes::span Generator::Part::grow(int size) {
 	if (_error
 		|| size <= 0
-		|| _result.size() + size > kClientHelloLimit) {
+		|| _result.size() + size > kClientHelloLength) {
 		_error = true;
 		return bytes::span();
 	}
@@ -381,10 +587,21 @@ void Generator::Part::writeBlocks(const QVector<MTPTlsBlock> &blocks) {
 }
 
 void Generator::Part::writeBlock(const MTPTlsBlock &data) {
-	data.match([&](const auto &data) {
-		writeBlock(data);
-	});
+    data.match(
+        [&](const MTPDtlsBlockString &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockZero &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockGrease &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockRandom &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockDomain &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockPublicKey &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockScope &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockPermutation &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockM &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockE &data) { writeBlock(data); },
+        [&](const MTPDtlsBlockPadding &data) { writeBlock(data); }
+    );
 }
+
 
 void Generator::Part::writeBlock(const MTPDtlsBlockString &data) {
 	const auto &bytes = data.vdata().v;
@@ -470,7 +687,7 @@ void Generator::Part::writeBlock(const MTPDtlsBlockPermutation &data) {
 		}
 		list.push_back(part.take());
 	}
-	ranges::shuffle(list);
+
 	for (const auto &element : list) {
 		const auto storage = grow(element.size());
 		if (storage.empty()) {
@@ -481,43 +698,14 @@ void Generator::Part::writeBlock(const MTPDtlsBlockPermutation &data) {
 }
 
 void Generator::Part::writeBlock(const MTPDtlsBlockM &data) {
-	constexpr auto kElements = 384;
-	constexpr auto kAdded = 32;
-
-	const auto storage = grow(kElements * 3 + kAdded);
-	if (storage.empty()) {
-		return;
-	}
-
-	auto random = bytes::vector(kElements * 8 + kAdded);
-	bytes::set_random(random);
-
-	auto chars = reinterpret_cast<char*>(storage.data());
-	const auto ints = reinterpret_cast<const uint32*>(random.data());
-	for (auto i = 0; i < kElements; ++i) {
-		const auto a = int(ints[i * 2] % 3329);
-		const auto b = int(ints[i * 2 + 1] % 3329);
-		*chars++ = (char)(a & 255);
-		*chars++ = (char)((a >> 8) + ((b & 15) << 4));
-		*chars++ = (char)(b >> 4);
-	}
-	bytes::set_random(storage.subspan(kElements * 3));
 }
 
 void Generator::Part::writeBlock(const MTPDtlsBlockE &data) {
-	const auto lengths = std::array{ 144, 176, 208, 240 };
-	const auto length = lengths[base::RandomIndex(lengths.size())];
-	writeBlock(MTP_tlsBlockRandom(MTP_int(length)));
 }
 
 void Generator::Part::writeBlock(const MTPDtlsBlockPadding &data) {
-	const auto length = int(_result.size());
-	if (length < 513) {
-		const auto zero = MTP_tlsBlockZero(MTP_int(513 - length));
-		writeBlock(MTP_tlsBlockString(MTP_bytes("\x00\x15"_q)));
-		writeBlock(MTP_tlsBlockScope(MTP_vector<MTPTlsBlock>(1, zero)));
-	}
 }
+
 
 void Generator::Part::finalize(bytes::const_span key) {
 	if (_error) {
@@ -526,6 +714,7 @@ void Generator::Part::finalize(bytes::const_span key) {
 		_error = true;
 		return;
 	}
+	writePadding();
 	writeDigest(key);
 	injectTimestamp();
 }
@@ -535,6 +724,14 @@ QByteArray Generator::Part::extractDigest() const {
 		return {};
 	}
 	return _result.mid(_digestPosition, kHelloDigestLength);
+}
+
+void Generator::Part::writePadding() {
+	Expects(_result.size() <= kClientHelloLength - kLengthSize);
+
+	const auto padding = kClientHelloLength - kLengthSize - _result.size();
+	writeBlock(MTP_tlsBlockScope(
+		MTP_vector<MTPTlsBlock>(1, MTP_tlsBlockZero(MTP_int(padding)))));
 }
 
 void Generator::Part::writeDigest(bytes::const_span key) {
@@ -553,7 +750,10 @@ void Generator::Part::injectTimestamp() {
 		sizeof(int32));
 	auto already = int32();
 	bytes::copy(bytes::object_as_span(&already), storage);
-	already ^= qToLittleEndian(int32(base::unixtime::http_now()));
+	auto randomJitter = int32(0);
+	bytes::set_random(bytes::object_as_span(&randomJitter));
+	randomJitter &= 0x3F;
+	already ^= qToLittleEndian(int32(base::unixtime::http_now() + randomJitter));
 	bytes::copy(storage, bytes::object_as_span(&already));
 }
 
@@ -563,7 +763,9 @@ Generator::Generator(
 	bytes::const_span key)
 : _greases(PrepareGreases())
 , _result(domain, _greases) {
-	_result.writeBlocks(rules.data().vblocks().v);
+	_result.writeBlocks(rules.match([&](const MTPDtlsClientHello &data) {
+		return data.vblocks().v;
+	}));
 	_result.finalize(key);
 }
 
@@ -594,7 +796,7 @@ ClientHello Generator::take() {
 		*reinterpret_cast<const uint16*>(storage.data()));
 }
 
-} // namespace
+}
 
 TlsSocket::TlsSocket(
 	not_null<QThread*> thread,
@@ -664,9 +866,28 @@ void TlsSocket::plainConnected() {
 	} else {
 		_state = State::WaitingHello;
 		_incoming = hello.digest;
-		_socket.write(hello.data);
+		
+		const auto &data = hello.data;
+		int offset = 0;
+		
+		auto rng = std::mt19937(std::random_device{}());
+		auto chunkSizeDist = std::uniform_int_distribution<int>(48, 96);
+		auto delayDist = std::uniform_int_distribution<int>(1, 5);
+		
+		while (offset < data.size()) {
+			const auto chunkSize = std::min(
+				chunkSizeDist(rng),
+				data.size() - offset);
+			_socket.write(data.mid(offset, chunkSize));
+			offset += chunkSize;
+			
+			if (offset < data.size()) {
+				QThread::msleep(delayDist(rng));
+			}
+		}
 	}
 }
+
 
 void TlsSocket::plainDisconnected() {
 	_state = State::NotConnected;
